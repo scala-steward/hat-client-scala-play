ef6758d0566dbf34994c4cc6115121d2bb1ac48e
andrius <andrius.aucinas@gmail.com>
2017-02-08 18:52:40 +0000

    adds correct public license information

--- src/main/scala/org/hatdex/hat/api/models/DataDebitOperations.scala
+++ src/main/scala/org/hatdex/hat/api/models/DataDebitOperations.scala
@@ -1,22 +1,10 @@
 /*
- * Copyright (C) 2016 Andrius Aucinas <andrius.aucinas@hatdex.org>
- * SPDX-License-Identifier: AGPL-3.0
+ * Copyright (C) 2016 HAT Data Exchange Ltd - All Rights Reserved
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Written by Andrius Aucinas <andrius.aucinas@hatdex.org>, 2 / 2017
  *
- * This file is part of the Hub of All Things project (HAT).
- *
- * HAT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation, version 3 of
- * the License.
- *
- * HAT is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
- * the GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General
- * Public License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
  */
 
 package org.hatdex.hat.api.models
