9d5c37bca3afca30a5b586860cca6ff5406fa59c
Terry Lee <terry.lee@hatdex.org>
2018-08-16 18:07:56 +0100

    Reverted HatApplications and bump version up to 2.6.3-SNAPSHOT

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -18,7 +18,7 @@ object BuildSettings extends AutoPlugin {
 
   override def projectSettings = Seq(
     organization := "org.hatdex",
-    version := "2.6.2-SNAPSHOT",
+    version := "2.6.3-SNAPSHOT",
     resolvers ++= Dependencies.resolvers,
     scalaVersion := Dependencies.Versions.scalaVersion,
     crossScalaVersions := Dependencies.Versions.crossScala,
---
+++ src/main/scala/org/hatdex/hat/api/services/HatApplications.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2016 HAT Data Exchange Ltd - All Rights Reserved
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Written by Andrius Aucinas <andrius.aucinas@hatdex.org>, 2 / 2017
+ *
+ */
+
+package org.hatdex.hat.api.services
+
+import org.hatdex.hat.api.models.HatService
+import org.hatdex.hat.api.services.Errors.{ ApiException, UnauthorizedActionException }
+import play.api.Logger
+import play.api.http.Status._
+import play.api.libs.json.{ JsError, JsSuccess, Json }
+import play.api.libs.ws._
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+trait HatApplications {
+  protected val logger: Logger
+  protected val ws: WSClient
+  protected val schema: String
+  protected val hatAddress: String
+  protected val host: String = if (hatAddress.isEmpty) "mock" else hatAddress
+
+  import org.hatdex.hat.api.json.HatJsonFormats._
+
+  def getApplications(access_token: String)(implicit ec: ExecutionContext): Future[Seq[HatService]] = {
+
+    val request: WSRequest = ws.url(s"$schema$hatAddress/api/v2/application")
+      .withVirtualHost(host)
+      .withHttpHeaders("Accept" -> "application/json", "X-Auth-Token" -> access_token)
+
+    val futureResponse: Future[WSResponse] = request.get()
+
+    futureResponse.flatMap { response =>
+      response.status match {
+        case OK =>
+          response.json.validate[Seq[HatService]] match {
+            case s: JsSuccess[Seq[HatService]] => Future.successful(s.get)
+            case e: JsError =>
+              logger.error(s"Error parsing Application listing response: $e")
+              Future.failed(new ApiException(s"Error parsing Application listing response: $e"))
+          }
+        case FORBIDDEN =>
+          Future.failed(UnauthorizedActionException(s"Getting applications for hat $hatAddress forbidden"))
+        case _ =>
+          logger.error(s"Listing applications for $hatAddress failed, $response, ${response.body}")
+          Future.failed(new ApiException(s"Listing applications for $hatAddress failed unexpectedly"))
+      }
+    }
+  }
+
+  def saveApplication(access_token: String, application: HatService)(implicit ec: ExecutionContext): Future[HatService] = {
+    val request: WSRequest = ws.url(s"$schema$hatAddress/api/v2/application")
+      .withVirtualHost(host)
+      .withHttpHeaders("Accept" -> "application/json", "X-Auth-Token" -> access_token)
+
+    val futureResponse: Future[WSResponse] = request.post(Json.toJson(application))
+
+    futureResponse.flatMap { response =>
+      response.status match {
+        case OK =>
+          response.json.validate[HatService] match {
+            case s: JsSuccess[HatService] => Future.successful(s.get)
+            case e: JsError =>
+              logger.error(s"Error parsing response from a successful application update: $e")
+              Future.failed(new ApiException(s"Error parsing response from a successful application update: $e"))
+          }
+        case FORBIDDEN =>
+          Future.failed(UnauthorizedActionException(s"Saving application for hat $hatAddress forbidden"))
+        case _ =>
+          logger.error(s"Saving application for $hatAddress failed, $response, ${response.body}")
+          Future.failed(new ApiException(s"Saving application for $hatAddress failed unexpectedly"))
+      }
+    }
+  }
+
+}
