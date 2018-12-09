c3ec44ed0cdc721143053ea325b6cf309532b5b0
Terry Lee <terry.lee@hatdex.org>
2018-12-09 22:43:04 +0000

    Bump to 2.6.5. Travis with 2.6.4 fails with HAT project

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -18,7 +18,7 @@ object BuildSettings extends AutoPlugin {
 
   override def projectSettings = Seq(
     organization := "org.hatdex",
-    version := "2.6.4-SNAPSHOT",
+    version := "2.6.5-SNAPSHOT",
     resolvers ++= Dependencies.resolvers,
     scalaVersion := Dependencies.Versions.scalaVersion,
     crossScalaVersions := Dependencies.Versions.crossScala,
