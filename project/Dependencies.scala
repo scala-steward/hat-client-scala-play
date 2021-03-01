ffd2163613d1dbcca5ce869d825d95fea1c7cf90
Scala Steward <me@scala-steward.org>
2021-03-01 18:31:17 +0100

    Update scala-library to 2.13.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val crossScala    = Seq("2.13.3", "2.12.12")
+    val crossScala    = Seq("2.13.5", "2.12.12")
     val scalaVersion  = crossScala.head
     val playVersion   = play.core.PlayVersion.current
     val testCommon    = "0.2.3"
